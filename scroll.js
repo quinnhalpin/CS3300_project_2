321d011007ffc1658f8c00a6e14720eb8c25349c
Jacob Gleberman <jhg279@cornell.edu>
2017-04-17 20:16:36 -0400

    Added the Scroll Javascript functions

---
+++ scroll.js
@@ -0,0 +1,238 @@
+
+/**
+ * scroller - handles the details
+ * of figuring out which section
+ * the user is currently scrolled
+ * to.
+ */
+
+function scroller() {
+  var container = d3.select('body');
+  // event dispatcher
+  var dispatch = d3.dispatch('active', 'progress');
+
+  var sections = null;
+
+ 
+  var sectionPositions = [];
+  var currentIndex = -1;
+  // y coordinate of
+  var containerStart = 0;
+
+  //Monitors the scrolling that occurs
+  function scroll(els) {
+    sections = els;
+
+    // when window is scrolled call
+    // position. When it is resized
+    // call resize.
+    d3.select(window)
+      .on('scroll.scroller', position)
+      .on('resize.scroller', resize);
+
+    // manually call resize
+    // initially to setup
+    // scroller.
+    resize();
+
+    // hack to get position
+    // to be called once for
+    // the scroll position on
+    // load.
+    // @v4 timer no longer stops if you
+    // return true at the end of the callback
+    // function - so here we stop it explicitly.
+    var timer = d3.timer(function () {
+      position();
+      timer.stop();
+    });
+  }
+
+  /**
+   * resize - called initially and
+   * also when page is resized.
+   * Resets the sectionPositions
+   *
+   */
+  function resize() {
+    // sectionPositions will be each sections
+    // starting position relative to the top
+    // of the first section.
+    sectionPositions = [];
+    var startPos;
+    sections.each(function (d, i) {
+      var top = this.getBoundingClientRect().top;
+      if (i === 0) {
+        startPos = top;
+      }
+      sectionPositions.push(top - startPos);
+    });
+    containerStart = container.node().getBoundingClientRect().top + window.pageYOffset;
+  }
+
+  /**
+   * position - get current users position.
+   * if user has scrolled to new section,
+   * dispatch active event with new section
+   * index.
+   *
+   */
+  function position() {
+    var pos = window.pageYOffset - 10 - containerStart;
+    var sectionIndex = d3.bisect(sectionPositions, pos);
+    sectionIndex = Math.min(sections.size() - 1, sectionIndex);
+
+    if (currentIndex !== sectionIndex) {
+      // @v4 you now `.call` the dispatch callback
+      dispatch.call('active', this, sectionIndex);
+      currentIndex = sectionIndex;
+    }
+
+    var prevIndex = Math.max(sectionIndex - 1, 0);
+    var prevTop = sectionPositions[prevIndex];
+    var progress = (pos - prevTop) / (sectionPositions[sectionIndex] - prevTop);
+    // @v4 you now `.call` the dispatch callback
+    dispatch.call('progress', this, currentIndex, progress);
+  }
+
+  /**
+   * container - get/set the parent element
+   * of the sections. Useful for if the
+   * scrolling doesn't start at the very top
+   * of the page.
+   *
+   * @param value - the new container value
+   */
+  scroll.container = function (value) {
+    if (arguments.length === 0) {
+      return container;
+    }
+    container = value;
+    return scroll;
+  };
+
+  // @v4 There is now no d3.rebind, so this implements
+  // a .on method to pass in a callback to the dispatcher.
+  scroll.on = function (action, callback) {
+    dispatch.on(action, callback);
+  };
+
+  return scroll;
+}
+
+
+
+
+var scrollVis = function () {
+  // constants to define the size
+  // and margins of the vis area.
+  var width = 600;
+  var height = 520;
+  var margin = { top: 0, left: 20, bottom: 40, right: 10 };
+
+  var lastIndex = -1;
+  var activeIndex = 0;
+
+  // Sizing for the grid visualization
+  var squareSize = 6;
+  var squarePad = 2;
+  var numPerRow = width / (squareSize + squarePad);
+
+  // main svg used for visualization
+  var svg = null;
+
+  // d3 selection that will be used
+  // for displaying visualizations
+  var g = null;
+
+  // When scrolling to a new section
+  // the activation function for that
+  // section is called.
+  var activateFunctions = [];
+
+  // If a section has an update function
+  // then it is called while scrolling
+  // through the section with the current
+  // progress through the section.
+
+  var updateFunctions = [];
+
+  /**
+   * setupSections - each section is activated
+   * by a separate function. Here we associate
+   * these functions to the sections based on
+   * the section's index.
+   *
+   */
+  var setupSections = function () {
+    // activateFunctions are called each
+    // time the active section changes
+    activateFunctions[0] = showTitle;
+
+    // updateFunctions are called while
+    // in a particular section to update
+    // the scroll progress in that section.
+    // Most sections do not need to be updated
+    // for all scrolling and so are set to
+    // no-op functions.
+    for (var i = 0; i < 1; i++) {
+      updateFunctions[i] = function () {};
+    }
+  };
+  /**
+   * showTitle - initial title
+   *
+   * hides: count title
+   * (no previous step to hide)
+   * shows: intro title
+   *
+   */
+  function showTitle() {
+    g.selectAll('.count-title')
+      .transition()
+      .duration(0)
+      .attr('opacity', 0);
+
+    g.selectAll('.openvis-title')
+      .transition()
+      .duration(600)
+      .attr('opacity', 1.0);
+  }
+
+/**
+ * display - called once data
+ * has been loaded.
+ * sets up the scroller and
+ * displays the visualization.
+ *
+ * @param data - loaded tsv data
+ */
+function display(data) {
+  // create a new plot and
+  // display it
+  var plot = scrollVis();
+  d3.select('#vis')
+    .datum(data)
+    .call(plot);
+
+  // setup scroll functionality
+  var scroll = scroller()
+    .container(d3.select('#graphic'));
+
+  // pass in .step selection as the steps
+  scroll(d3.selectAll('.step'));
+
+  // setup event handling
+  scroll.on('active', function (index) {
+    // highlight current step text
+    d3.selectAll('.step')
+      .style('opacity', function (d, i) { return i === index ? 1 : 0.1; });
+
+    // activate current section
+    plot.activate(index);
+  });
+
+  scroll.on('progress', function (index, progress) {
+    plot.update(index, progress);
+  });
+}
